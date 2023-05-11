59d339cc4010b9909445a0e85adcbae894c69370
Jesus Marron <jesusmarron7@gmail.com>
2023-05-11 14:52:01 -0500

    basic form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>Forms</h1>>
+<h1>Forms</h1>
+
+<form method="POST" action="/login">
+	<input type="text" name="user" />
+	<input type="submite" name="password" />
+	<button type="submit">Login</button>
+</form>
